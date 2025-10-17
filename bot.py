b3b0947bd6076b82ce381209b49ae35ec3255a13
itxmahboob110 <147175733+itxmahboob110@users.noreply.github.com>
2025-10-17 08:42:57 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive."
+
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 import asyncio
 import datetime
 import os
@@ -170,4 +187,5 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    keep_alive()
     asyncio.run(main())
