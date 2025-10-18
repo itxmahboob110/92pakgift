ca9dd628f9f601ca0bf32097b821236fa86c7648
itxmahboob110 <147175733+itxmahboob110@users.noreply.github.com>
2025-10-18 10:24:56 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -187,3 +187,16 @@ if __name__ == "__main__":
         await asyncio.Event().wait()
 
     asyncio.run(run())
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "92Pak Telegram Bot Running 🚀"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
